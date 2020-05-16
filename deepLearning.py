c8a7efd023de92ca2287f63485847b47ceb1cf9a
gogettp11 <gogetoss@gmail.com>
2020-05-16 13:15:58 +0200

    done

---
+++ deepLearning.py
@@ -0,0 +1,56 @@
+import random
+
+def eval(list):
+    val1,val2 = list
+    return (val1**2-val2**2)-(1-val1)**2
+
+def genrateNewPopulation(maxSpan,minSpan,popAmount,bestone,leap):
+    pop = []
+    minx = bestone[0] - leap
+    maxx = bestone[0] + leap
+    miny = bestone[1] - leap
+    maxy = bestone[1] + leap
+    if minx < minSpan:
+        minx = minSpan
+    if maxx > maxSpan:
+        maxx = maxSpan
+    if miny < minSpan:
+        miny = minSpan
+    if maxy > maxSpan:
+        maxy = maxSpan
+    for obj in range(0,popAmount):
+        obj = [random.uniform(minx,maxx),random.uniform(miny,maxy)]
+        pop.append(obj)
+    return pop
+
+def main():
+
+    pop = []
+    bestone = None
+    bestscore = float("-inf")
+    maxSpan = 200
+    minSpan = -200
+    popAmount = 10
+    leap = 10
+    generations = 100
+
+    for obj in range(0,popAmount):
+        obj = [float(random.randint(minSpan,maxSpan)),float(random.randint(minSpan,maxSpan))]
+        pop.append(obj)
+
+    for generation in range(0,generations):
+        print("Generation:" + generation.__str__() + "\n" + "Creatures: " + pop.__str__())
+        for obj in pop:
+            individual = eval(obj)
+            if individual > bestscore:
+                bestone = obj
+                bestscore = individual
+        #generate new generation
+        print("best in generation: " + bestone.__str__())
+        print("best score: " + bestscore.__str__() + "\n")
+        pop = genrateNewPopulation(maxSpan,minSpan,popAmount,bestone,leap)
+
+
+if __name__=="__main__":
+    main()
+
